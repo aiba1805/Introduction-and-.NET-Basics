0b968fa99b55038e25602eff563979fc53bea70c
Aibek Shulembekov <aibek1805@gmail.com>
2019-10-12 01:20:34 +0600

    Added projects Done 1-5 Basic Coding tasks

---
+++ BasicCoding/Tasks.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using Microsoft.VisualBasic;
+
+namespace BasicCoding
+{
+    public class Tasks
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new[] {4, 5, 22, 12, 3, 34, 56, 99};
+            Console.WriteLine("Массив:");
+            arr.ToList().ForEach(x => Console.Write($"{x} "));
+            Console.WriteLine($"\nМаксимальный элемент массива: {Max(arr, arr.Length)}");
+        }
+
+        /// <summary>
+        ///  Метод вставляет биты первого числа с j по i в биты второго числа с j по i.
+        /// С помощью методов класса Convert конвертируем число в двоичное значение в виде строки,
+        /// а затем в массив, затем переворачиваем его так как, в двоичном исчислении нумерация начинается справа,
+        /// Затем обратно в массив, чтобы можно было с ним работать.
+        /// Далее следует цикл, который соотвественно меняет биты.
+        /// </summary>
+        /// <param name="numberSource"></param>
+        /// <param name="numberIn"></param>
+        /// <param name="j"></param>
+        /// <param name="i"></param>
+        /// <returns> конвертируем массив символов перевернутый, преобразованный в строку, в качестве системы измерения задаем двоичную </returns>
+        public static int InsertNumber(int numberSource, int numberIn, int j, int i)
+        {
+            var sourceString = Convert.ToString(numberSource, 2)
+                .ToCharArray().Reverse().ToArray();
+            var inString = Convert.ToString(numberIn, 2)
+                .ToCharArray().Reverse().ToArray();
+            for (int k = j; k <= i; k++)
+            {
+                inString[k] = sourceString[k];
+            }
+
+            return Convert.ToInt32(new string(inString.Reverse().ToArray()), 2);
+        }
+
+        /// <summary>
+        /// Если индекс равен 1, то возращаем первый элемент массива.
+        /// Затем рекурсивно вызываем функцию и передаем туда индекс на один меньше.
+        /// Сравниваем на большое значение элемент который мы получили из рекурсивного вызова и и текущий элемент.
+        /// </summary>
+        /// <param name="arr">Массив чисел</param>
+        /// <param name="n">Индекс</param>
+        /// <returns>Возращает максимальное значение</returns>
+        public static int Max(int[] arr, int n)
+        {
+            if (n is 1)
+                return arr[0];
+            var max = Max(arr, n - 1);
+            return arr[n - 1] > max ? arr[n - 1] : max;
+        }
+        
+        /// <summary>
+        /// Проходимся по массиву с помощью foreach, для каждого значения 
+        /// получаем индекс с помощью LINQ функции IndexOf, пред этим конвертируем
+        /// массив в лист.
+        /// Считаем сумму до и после индекса, выбирая элементы с помощью LINQ функции Where
+        /// Сравниваем суммы.
+        /// </summary>
+        /// <param name="arr">Массив вещественных чисел</param>
+        /// <returns>Возращает целое число, индекс массива, если такого числа нет возвращает -1.</returns>
+        public static int FindByEqualSum(float[] arr)
+        {
+            foreach (var i in arr)
+            {
+                var indexOfCur = arr.ToList().IndexOf(i);
+                var sumBefore = arr.Where(x => arr.ToList().IndexOf(x) < indexOfCur).Sum();
+                var sumAfter = arr.Where(x => arr.ToList().IndexOf(x) > indexOfCur).Sum();
+                if (sumBefore == sumAfter)
+                    return indexOfCur;
+            }
+
+            return -1;
+        }
+
+        /// <summary>
+        /// Проверяем строки на содержание только латинских букв.
+        /// Анонимно создаем новую строку из строки у которой с помощью функции Distinct
+        /// удаляем повторяющиеся элементы.
+        /// С помощью перегурженного оператора + складываем строки 
+        /// </summary>
+        /// <param name="first">Первая строка</param>
+        /// <param name="second"Вторая строка</param>
+        /// <returns></returns>
+        public static string Concat(string first, string second)
+        {
+            if(!(CheckerAZ(first) && CheckerAZ(second))) throw new ArgumentException("String contains non alphabetical characters!");
+            return new string(first.Distinct().ToArray())
+                + new string(second.Distinct().ToArray());
+        }
+        /// <summary>
+        /// Функция которая проверяет содержит ли строка
+        /// только латинские букву в противном случае возвращает false.
+        /// </summary>
+        /// <param name="str">проверяемая строка</param>
+        /// <returns></returns>
+        private static bool CheckerAZ(string str)
+        {
+            foreach (var i in str)
+            {
+                var c = Char.ToLower(i);
+                if (!(c >= 'a' && c <= 'z'))
+                    return false;
+            }
+
+            return true;
+        }
+
+        public static (int,Stopwatch) FindNextBiggerNumber(int num)
+        {
+            var s = new Stopwatch();
+            s.Start();
+            var digits = IntToDigits(num);
+            var j = digits.Length - 2;
+            while (j != -1 && digits[j] >= digits[j + 1]) j--;
+            if (j == -1)
+                return (-1,s); 
+            var k = digits.Length - 1;
+            while (digits[j] >= digits[k]) k--;
+            Swap(ref digits, j, k);
+            int l = j + 1, r = digits.Length - 1; 
+            while (l<r) 
+                Swap(ref digits, l++, r--);
+            s.Start();
+            return (DigitsToInt(digits),s);
+        }
+
+        private static int[] IntToDigits(int num)
+        {
+            var digits = new List<int>();
+            foreach (var c in num.ToString().ToArray())
+            {
+                Int32.TryParse(c.ToString(), out int value);
+                digits.Add(value);
+            }
+
+            return digits.ToArray();
+        }
+
+        private static int DigitsToInt(int[] arr)
+        {
+            int value = 0;
+            for (int i = 0; i < arr.Length; ++i)
+            {
+                value *= 10;
+                value += arr[i];
+            }
+
+            return value;
+        }
+
+        private static void Swap<T>(ref T[] arr, int i, int j)
+        {
+            T c = arr[i];
+            arr[i] = arr[j];
+            arr[j] = c;
+        }
+    }
+}
---
+++ Tests/BasicCoding.cs
@@ -0,0 +1,68 @@
+using System;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using BasicCoding;
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class BasicCoding
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test_InsertNumber()
+        {
+            //arrange
+            var sourceNum = 255;
+            var inNum = 200;
+            var i = 6;
+            var j = 3;
+            //act
+            var result = Tasks.InsertNumber(sourceNum, inNum, j, i);
+
+            Assert.AreEqual(result, 248);
+        }
+
+        [Test]
+        public void Test_FindByEqualSum()
+        {
+            float[] arr = {22.3f, 44, 41, 35.2f, 31.1f};
+
+            var res = Tasks.FindByEqualSum(arr);
+            
+            Assert.AreEqual(res,2);
+        }
+        
+        [Test]
+        public void Test_Concat()
+        {
+            string first = "Hello", second = "Worldd";
+
+            var res = Tasks.Concat(first,second);
+            
+            Assert.AreEqual(res,"HeloWorld");
+        }
+
+        [TestCase(12,21)]
+        [TestCase(513,531)]
+        [TestCase(2017,2071)]
+        [TestCase(414,441)]
+        [TestCase(144,414)]
+        [TestCase(1234321,1241233)]
+        [TestCase(1234126,1234162)]
+        [TestCase(3456432,3462345)]
+        [TestCase(10, -1)]
+        [TestCase(20, -1)]
+        public void Test_FindNextBiggerNumber(int number, int expected)
+        {
+            var result = Tasks.FindNextBiggerNumber(number);
+            Console.Write($"Time elapsed: {result.Item2.Elapsed}");
+            Assert.AreEqual(result.Item1,expected);
+        }
+        
+        
+    }
+}
